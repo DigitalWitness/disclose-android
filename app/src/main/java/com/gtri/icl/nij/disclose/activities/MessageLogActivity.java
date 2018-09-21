bd5e7ce59b8732bf0807faf256e873ff2b305102
Russell <russell@russell-research.com>
2018-09-21 15:45:45 -0400

    updated header images

--- app/src/main/java/com/gtri/icl/nij/disclose/activities/MessageLogActivity.java
+++ app/src/main/java/com/gtri/icl/nij/disclose/activities/MessageLogActivity.java
@@ -34,7 +34,7 @@ public class MessageLogActivity extends BaseActivity implements RecyclerItemTouc
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_device_log);
+        setContentView(R.layout.activity_message_log);
 
         setCustomTitle("");
 
